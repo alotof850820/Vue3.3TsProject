15ca0d577360dc2927c952b3a1779a50a165064e
Wun-Siang Huang <alotof840114@gmail.com>
2023-07-31 20:35:59 +0800

    add

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,30 +1,26 @@
-//进行axios二次封装:使用请求与响应拦截器
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import useUserStore from "@/store/modules/user";
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_API, //基础路径上会携带/api
+  baseURL: import.meta.env.VITE_APP_BASE_API, //带/api
+  timeout: 5000,
 });
-//添加请求与响应拦截器
+
 request.interceptors.request.use((config) => {
-  //获取仓库内部token,登录成功以后携带给服务器
   const userStore = useUserStore();
   if (userStore.token) {
     config.headers.token = userStore.token;
   }
-  //config配置对象,headers属性请求头,经常给服务器端携带公共参数
   return config;
 });
 
-//响应拦截器
 request.interceptors.response.use(
   (response) => {
     return response.data;
   },
   (error) => {
     let message = "";
-    //http状态码
 
     const status = error.code;
 
